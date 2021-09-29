0569f534681daf22f61e1c26d4f8e450113df060
tripatsandhu <91009086+tripatsandhu@users.noreply.github.com>
2021-09-29 19:25:11 +0530

    Update app.py
    Adding comments

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template,url_for,request
-import pickle
-import numpy as np
+from flask import Flask,render_template,url_for,request #requirments flask app
+import pickle#import pickle
+import numpy as np#numpy
 
-model=pickle.load(open('model.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))#imported model
 
-app = Flask(__name__)
+app = Flask(__name__)#normal syntax
 
 @app.route("/")
 def home():
@@ -12,14 +12,14 @@ def home():
 
 @app.route("/predict",methods = ["POST","GET"])
 def predict():
-    int_features =[float(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    prediction = model.predict(final)
-    output = prediction[0]
+    int_features =[float(x) for x in request.form.values()]#fetching values from the html form
+    final=[np.array(int_features)]#converting raw string into numpy array
+    prediction = model.predict(final)#model prediction
+    output = prediction[0]#saved into output variable 
     if output == 1:
         return render_template('home.html',pred = "You may high chances of being diabetic")
     else:
         return render_template("home.html",pred = "The chances that you have diabetes are very less")
 
 if __name__ == "__main__":
-       app.run(debug = True)
+       app.run(debug = True)#normal syntax
